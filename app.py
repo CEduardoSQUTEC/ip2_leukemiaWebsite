12bba87ebf744562bd9b8eebd4120293c3fad787
CEduardoSQUTEC <eduardo.castro@utec.edu.pe>
2020-11-25 18:26:36 -0500

    Webpage structure working

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-a == Flask(__name__)
-
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, world!"
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
