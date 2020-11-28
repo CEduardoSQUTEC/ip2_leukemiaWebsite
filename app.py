fe783c83ed8f09d02f7147b6384166671764ac7c
CEduardoSQUTEC <eduardo.castro@utec.edu.pe>
2020-11-28 17:40:08 -0500

    web page in process

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('i.html')
+
+@app.route('/countcells', methods=['POST'])
+def countCells():
+    image = request.form['img']
+    # return "<img src={} alt=\"Image\">".format(image)
+    return "<h1>OK</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
